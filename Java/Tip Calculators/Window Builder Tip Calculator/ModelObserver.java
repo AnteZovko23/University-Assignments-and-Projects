07e099426b16c980a060d3b15d75e746178b43b4
Ante Zovko <antezovko.az@gmail.com>
2019-05-09 16:54:25 -0400

    Add files via upload

---
+++ Java/Tip Calculators/Window Builder Tip Calculator/ModelObserver.java
@@ -0,0 +1,14 @@
+
+// TODO
+
+/**
+ * ModelObserver is a simple interface that all observers of the Model class must implement
+ * 
+ * @author dlevine
+ *
+ */
+public interface ModelObserver {
+    
+    public void update(Model m);
+    
+}
