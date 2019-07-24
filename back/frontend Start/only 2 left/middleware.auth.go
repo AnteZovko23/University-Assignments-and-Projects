1e03b1f19e3d31e7ee2cc82603cfdef876fbfc8c
Ante Zovko <antezovko.az@gmail.com>
2019-07-24 16:00:23 +0200

    Add files via upload

---
+++ back/frontend Start/only 2 left/middleware.auth.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// This middleware ensures that a request will be aborted with an error
+// if the user is not logged in
+func ensureLoggedIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// If there's an error or if the token is empty
+		// the user is not logged in
+		loggedInInterface, _ := c.Get("is_logged_in")
+		loggedIn := loggedInInterface.(bool)
+		if !loggedIn {
+			//if token, err := c.Cookie("token"); err != nil || token == "" {
+			c.Redirect(http.StatusTemporaryRedirect, "/users/login")
+		}
+	}
+}
+
+// This middleware ensures that a request will be aborted with an error
+// if the user is already logged in
+func ensureNotLoggedIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// If there's no error or if the token is not empty
+		// the user is already logged in
+		loggedInInterface, _ := c.Get("is_logged_in")
+		loggedIn := loggedInInterface.(bool)
+		if loggedIn {
+			// if token, err := c.Cookie("token"); err == nil || token != "" {
+			c.Redirect(http.StatusTemporaryRedirect, "/users/homepage")
+		}
+	}
+}
+
+// This middleware sets whether the user is logged in or not
+func setUserStatus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if token, err := c.Cookie("token"); err == nil || token != "" {
+			c.Set("is_logged_in", true)
+		} else {
+			c.Set("is_logged_in", false)
+		}
+	}
+}
