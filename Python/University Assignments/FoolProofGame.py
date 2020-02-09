93f81ce3c7fbf153d6011e45f8b027f5d186a699
zovkoante23 <antezovko.az@gmail.com>
2020-02-08 20:03:48 -0600

    New Projects

--- Python/University Assignments/FoolProofGame.py
+++ Python/University Assignments/FoolProofGame.py
@@ -1,15 +1,21 @@
 from random import randint, choice
 
+###########################################################################################
 # Name: Ante Zovko
-# Date 2/4/2020
-# TODO: Documentation
+# Date: October 8th, 2019
+# Description: A game of heads and tails with two coins where Group A gets a point if 
+#              the result is two heads, Group B gets a point if the result is two tails
+#              and the Prof gets a point if it is heads/tails or tails/heads
+###########################################################################################
 
+
+# The coin class
 class Coin(object):
 
     headsSide = "Heads"
     tailsSide = "Tails"
         
-
+    # Randomly returns heads or tails when called
     def flip(self):
         randomNumber = randint(0, 1)
 
@@ -18,30 +24,40 @@ class Coin(object):
         else:
             return self.tailsSide
 
+# Two coin objects
 firstCoin = Coin()
 secondCoin = Coin()
 
-
+# The game
+# Params: Number of games and tosses per each game
+# Returns: The result of every game as a list
 def game(numOfGames, tossesPerGame):
     groupA = 0
     groupB = 0
     prof = 0
     allGames = []
 
+    # The outer loop keeps track of games
     for i in range(numOfGames):   
+        # The inner loop keeps track of tosses in each game
         for j in range(tossesPerGame):
             groupA, groupB, prof = coinTossTracker(groupA, groupB, prof)
 
+        # Append each result to the list
         allGames.append(groupA)
         allGames.append(groupB)
         allGames.append(prof)
-
+        
+        # Reset scores for next game
         groupA, groupB, prof = 0, 0, 0
 
     return allGames
         
         
-
+# Keeps track of the score for the game
+# Params: The players' current score
+# Return: The players' current score and the
+# the player who won incremented by 1
 def coinTossTracker(groupA, groupB, prof):
     if(firstCoin.flip() == "Heads" and secondCoin.flip() == "Heads"):
         return groupA + 1, groupB, prof
@@ -52,7 +68,8 @@ def coinTossTracker(groupA, groupB, prof):
     else:
         return groupA, groupB, prof + 1
 
-
+# Handles Input from the client-side
+# Params: The message for the user
 def inputValidation(message):
     while True:
         try:
@@ -67,26 +84,40 @@ def inputValidation(message):
             print("Please enter a number!\n")
             continue
 
+# Creates the layout that displays the statistics
+# about the game(s)
+# Params: Number of games, tosses and the results of each game
 def outputLayout(numOfGames, numOfTosses, allGames):
 
+    # Keeps track of total wins
     groupAWins = 0
     groupBWins = 0
     profWins = 0
     
+    # Casts to float for division
     numOfTosses = float(numOfTosses)
     flNumOfGames = float(numOfGames)
 
+    # Displays each game and its statistics
     for i in range(numOfGames):
         print("Game {}\n Group A: {} ({}%); Group B: {} ({}%); Prof: {} ({}%);".format(i, allGames[i*3], round((allGames[i*3]/numOfTosses)*100, 2),allGames[(i*3)+1], 
         round((allGames[(i*3)+1]/numOfTosses)*100, 2),allGames[(i*3)+2], round((allGames[(i*3)+2]/numOfTosses)*100, 2)))
         
+        # Creates a list with the three current scores
         wins = [allGames[i*3], allGames[(i*3)+1], allGames[(i*3)+2]]
+        
+        # Checks if there are duplicates by counting how many maxes there are
         if(wins.count(max(wins)) > 1):
+            # Gets the duplicates positions in the list
             maxPositions = list_duplicates_of(wins, max(wins))
+            # Picks a random winner
             winner = choice(maxPositions)
         else:
+            # Winner is the player with the highest index
             winner = wins.index(max(wins))
 
+        # Increments total wins for the
+        # corresponding player
         if(winner == 0):
             groupAWins += 1
         elif(winner == 1):
@@ -94,12 +125,15 @@ def outputLayout(numOfGames, numOfTosses, allGames):
         elif(winner == 2):
             profWins += 1
 
+    # Displays the total statistics
     print("Wins: Group A: {} ({}%); Group B: {} ({}%); Prof: {} ({}%)".format(groupAWins, round((groupAWins/flNumOfGames)*100, 2), groupBWins, round((groupBWins/flNumOfGames)*100, 2),
     profWins, round((profWins/flNumOfGames)*100), 2))
 
 
-# Found on:
-# https://stackoverflow.com/questions/5419204/index-of-duplicates-items-in-a-python-list
+# Finds the positions of duplicates in a list
+# Params: list, item to find
+# Returns: List of duplicate positions
+# Found on: https://stackoverflow.com/questions/5419204/index-of-duplicates-items-in-a-python-list
 def list_duplicates_of(seq,item):
     start_at = -1
     locs = []
