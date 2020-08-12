eebf4b3dde0be74c10480d24f0b36fda479cc976
zovkoante23 <antezovko.az@gmail.com>
2020-08-12 14:11:37 +0200

    Server and Client1

--- JavaGrpcSpringBoot/server/src/main/java/com/grpcserver/server/Server.java
+++ JavaGrpcSpringBoot/server/src/main/java/com/grpcserver/server/Server.java
@@ -1,12 +1,15 @@
 package com.grpcserver.server;
 
 import com.grpcservice.proto.lib.*;
+import com.grpcservice.proto.lib.MyServiceGrpc.MyServiceImplBase;
+
 import net.devh.boot.grpc.server.service.GrpcService;
 import io.grpc.stub.StreamObserver;
 
 @GrpcService
-public class Server extends MyServiceGrpc.MyServiceImplBase {
+public class Server extends MyServiceImplBase {
     
+
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         HelloReply reply = HelloReply.newBuilder()
